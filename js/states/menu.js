bd17f289b53102dabcfac735cbf480d02bb8f31a
Hannah Ward <Hannah.ward9001@gmail.com>
2016-12-09 23:36:01 +0000

    Ready to start

---
+++ js/states/menu.js
@@ -0,0 +1,13 @@
+var Menu = function() {};
+Menu.prototype = {
+  init: function() {
+  },
+  start_game: function() {
+  },
+  preload: function() {
+  },
+  create: function() {
+  },
+  update: function() {
+  },
+};
