64571bf4861245b1a2ea9fb8712498fea4c56792
Hannah Ward <Hannah.ward9001@gmail.com>
2016-12-10 05:33:21 +0000

    OK that stupid beat thing actually works now

--- js/beat_util.js
+++ js/beat_util.js
@@ -7,10 +7,16 @@ var beatProc = function(){return{
   startTime: 0,
   // When the timer should stop
   stopTime: 0,
+  // The amount of leeway you get
+  tolerance: 500,
 
   // Set the expected times
   setExpectedTimes: function(times) {
     this.expectedTimes = times;
+  },
+
+  getExpectedTimes: function() {
+    return this.expectedTimes;
   },
 
   // Set the actual times
@@ -31,6 +37,7 @@ var beatProc = function(){return{
   // Register a click
   registerClick: function() {
     this.actualTimes.push(Date.now());
+    return this.processClicks();
   },
 
   // Figure out if we should stop now
@@ -38,52 +45,30 @@ var beatProc = function(){return{
     return (Date.now() >= this.stopTime);
   },
 
-  // Try to figure out if beats were missed
-  // Vulnerable to spam clicking, but hey
-  // Idea: Start with the actual clicks
-  //       For each expected time, find the click
-  //       it matches most closely.
-  alignClicks: function(expectedTimes) {
-    // This will always be the difference in the number of clicks
-    var dropped     = Math.abs(expectedTimes.length-this.actualTimes.length);
-    var variance    = 0;
-    for (var i in this.actualTimes) {
-      var actualTime = this.actualTimes[i];
-      var min = -1;
-      // For each expected time
-      for (var j in expectedTimes) {
-        // Get the absolute difference to the actual time
-        var expectedTime = expectedTimes[j];
-        var diff = Math.abs(actualTime-expectedTime);
-
-        // If it's smaller, save it
-        if (diff < Math.abs(min-actualTime)) {
-          min = expectedTime;
-        } 
-      } 
-
-      if (min != -1) {
-        console.log("Mapping ",actualTime," to ",min);
-        // Remove the minimum value so we can't map it twice    
-        expectedTimes.pop(min);
-        variance += Math.abs(min-actualTime);
-      }
-    }  
-  
-    return { variance : variance, dropped: dropped }
-  },
-
   // Figure out how far off the user's clicks were
   processClicks: function() {
     // Use the delays and the start time to find out the 
     // timestamp when the user should have clicked
     var adjustedExpectedTimes = [];
-    var adjust = 0;
+    var adjust = 0
+    var mapping = {};
     for (var i in this.expectedTimes) {
       var time = this.expectedTimes[i];
-      adjustedExpectedTimes.push(this.startTime + time + adjust);
+      adjustedExpectedTimes.push(this.startTime + adjust);
+    
+      mapping[time] = this.startTime + adjust; 
       adjust += time;
     }
-    console.log(this.alignClicks(adjustedExpectedTimes));
+    console.log(adjustedExpectedTimes, Date.now())
+    for (var i in adjustedExpectedTimes) {
+      if (Math.abs(Date.now() - adjustedExpectedTimes[i]) <= this.tolerance) {
+        this.expectedTimes = this.expectedTimes.filter(
+          (y)=>{return y != mapping[adjustedExpectedTimes[i]]}
+        );
+        return true;
+      } 
+      console.log(Math.abs(Date.now() - adjustedExpectedTimes[i]));
+    }
+    return false;
   },
 }}
--- js/states/build_room.js
+++ js/states/build_room.js
@@ -10,6 +10,8 @@ BuildRoom.prototype = {
   },
   preload: function() {
     this.game.load.image("test", "img/test.png");
+    this.game.load.audio("blip", "snd/Blip.wav");
+
   },
   create: function() {
     this.testSprite = this.game.add.sprite(100,100,"test");
@@ -18,15 +20,58 @@ BuildRoom.prototype = {
     this.beatUtil = new beatProc();
     this.beatUtil.setExpectedTimes([1000, 1000, 1000]);
 
-    this.beatUtil.start();
+    this.clickIsActive = false;
+    this.demo = true;
 
+    this.blip = this.game.add.audio("blip");
+    
+    this.countdown = this.game.add.text(0,0,"",{fill:"#ffffff",
+                                                font:"65px Arial"})
+  },
+
+  activateClick: function() {
     this.clickIsActive = true;
+    this.beatUtil.start();
+  },
+  gogogo: function() {
+    setTimeout(function(that){that.activateClick()},
+               3000,this);
+    setTimeout(function(that){that.setText("2")}, 1000, this.countdown)
+    setTimeout(function(that){that.setText("1")}, 2000, this.countdown)
+    setTimeout(function(that){that.setText("GO")}, 3000, this.countdown)
   },
   update: function() {
-    if (this.clickIsActive) {
+    // Show the user what rhythm to do
+    if (this.demo) {
+      var nullTween = {x : this.testSprite.x}
+      var total = 0;
+      var t =null;
+      this.beatUtil.getExpectedTimes().forEach(
+        (time) => {
+          console.log(time);
+          // Do nothing for some time, then boop
+          t = this.game.add.tween(this.testSprite).to({alpha:1}, 50,
+            "Linear", true, total + time - 100)
+          .chain(
+            this.game.add.tween(this.testSprite).to({alpha:0}, 50)
+          );
+          setTimeout(function(snd){snd.play()}, total+time-40, this.blip);
+          t.start();
+          total += time;
+        }
+      )
+      t.onComplete.add(this.gogogo, this);
+      
+      this.countdown.setText("3")
+      this.demo = false;
+    }
+    else if (this.clickIsActive) {
       if (this.game.input.activePointer.justPressed(25)) {
-        this.beatUtil.registerClick();
+        console.log(this.beatUtil.registerClick());
 
+        this.testSprite.x = this.game.input.activePointer.pageX;
+        this.testSprite.y = this.game.input.activePointer.pageY;
+        
         this.game.add.tween(this.testSprite)
         .to({ alpha: 1 }, 50)
         .chain(this.game.add.tween(this.testSprite)
@@ -37,8 +82,6 @@ BuildRoom.prototype = {
       if (this.beatUtil.doesStop()) {
         console.log("CLICK DISABLED!");
         this.clickIsActive = false;
-        var variance = this.beatUtil.processClicks();
-        console.log(variance);
       }
     }
          
