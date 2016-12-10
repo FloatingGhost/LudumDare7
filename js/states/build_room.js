f4044fd2546b5cb0f0ab4dd1fb41e36956cc7d38
Hannah Ward <Hannah.ward9001@gmail.com>
2016-12-10 22:28:03 +0000

    https://www.youtube.com/watch?v=fvtQYsckLxk

--- js/states/build_room.js
+++ js/states/build_room.js
@@ -19,23 +19,53 @@ BuildRoom.prototype = {
     this.game.load.audio("music", "snd/Level1.wav");
   },
   create: function() {
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
     this.music = this.game.add.audio("music"); 
-    this.game.time.events.add(428*4, this.loadLevel, this);
+    this.game.time.events.add(428*2, this.loadLevel, this);
+    this.hammer = this.game.add.sprite(100, 300, "hammer");
+    this.hammer.animations.add("hit");
+    this.game.physics.p2.enable(this.hammer);
+    this.hammer.body.onBeginContact.add(this.hammerHit, this);
+    this.hammer.body.collideWorldBounds = false;
+    this.game.input.onDown.add(this.hit, this);
     this.start_game();
     this.wood = this.game.add.group();
+    
+  },
+
+  hammerHit: function(body, bodyB, shapeA, shapeB, eqn) {
+    console.log("HITTY!");
   },
   
+  hit: function() {
+    this.hammer.animations.play("hit");
+    this.wood.forEach(
+      (item) => { 
+        if (item.DOTHETHING) {
+          console.log(item);
+          console.log(item.world.x);
+          if (item.world.x > 25 && item.world.x < 60) {
+            item.DOTHETHING = false;
+            item.kill();
+            console.log("YEAH BOIIII");
+          } 
+        }
+      }
+    )
+  },
 
   loadLevel: function() {
     this.wood.removeAll();
     this.wood.x = 0;
     this.currentLevel = levelConfig.levelConfig.splice(0, 1)[0];
-    this.wood.create(-400, 400, "wood");
     for (var i in this.currentLevel) {
       if (this.currentLevel[i]) {
-        this.wood.create(-(i*100)-30, 320, "nail");
+        var w =this.wood.create(-(i*100)+15, 330, "nail");
+        w.DOTHETHING=1;
       }
     } 
+
+    this.wood.create(-400, 400, "wood");
     // Wait for the demo to play, the move
     t= this.game.add.tween(this.wood).to({x:"+400"}, 428*4*2, "Linear",true, 
                                       428*4*2)
@@ -45,7 +75,7 @@ BuildRoom.prototype = {
   },
 
   woodFlyAway: function() {
-    this.game.add.tween(this.wood).to({x:"+800"}, 400).start();
+    this.game.add.tween(this.wood).to({x:"+800"}, 400, "Linear", true,100).start();
   },
 
   woodFinish: function() {
@@ -56,6 +86,6 @@ BuildRoom.prototype = {
   },
 
   processClick: function() {
-
+  
   },
 };
