a714320f03e272eda88d90186d42943bc87343ff
Hannah Ward <Hannah.ward9001@gmail.com>
2016-12-11 20:08:10 +0000

    Change menu

--- js/states/menu.js
+++ js/states/menu.js
@@ -8,12 +8,10 @@ Menu.prototype = {
   preload: function() {
     this.game.load.audio("voice", "snd/menusound.wav");
     this.game.load.image("bg", "img/menubg.png");
-    this.game.load.image("but", "img/dothedo.png");
   },
   create: function() {
     this.game.add.audio("voice").play();
     this.game.add.sprite(0,0,"bg");
-    var v = this.game.add.sprite(230, 280, "but");
     game.input.onTap.add(this.start_game, this);
   },
   update: function() {
